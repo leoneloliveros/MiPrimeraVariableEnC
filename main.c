54a31738ba10d062d1b585117aa1c4209e50aae2
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-06 13:25:28 -0500

    Challenge REsolved

--- main.c
+++ main.c
@@ -4,24 +4,14 @@
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
+    int value;
 
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
+    printf("Ingresa el valor: ");
+    scanf("%i", &value);
 
+    value %= 5;
 
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
-
+    printf("El resultado es: %i", ++value);
 
     return 0;
 }
