2c4444b2c8c893c86c6ecacb30289cbc8eca0570
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-06 20:51:01 -0500

    Learning Relational Operators

--- main.c
+++ main.c
@@ -1,26 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
+    printf("Operadores relacionales \n");
+    
+    float valA, valB;
+    
+    valA = 5;
+    valB = 10;
+    
+    if(valA < valB)
+        printf("Pregunta 1: Cierto \n");
+    else
+        printf("Pregunta 1: Falso \n");
+    
+    if(valA <= valB)
+            printf("Pregunta 2: Cierto \n");
+        else
+            printf("Pregunta 2: Falso \n");
+    
+    if(valA > valB)
+            printf("Pregunta 3: Cierto \n");
+        else
+            printf("Pregunta 3: Falso \n");
 
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
+    if(valA >= valB)
+            printf("Pregunta 4: Cierto \n");
+        else
+            printf("Pregunta 4: Falso \n");
 
+    if(valA == valB)
+            printf("Pregunta 5: Cierto \n");
+        else
+            printf("Pregunta 5: Falso \n");
 
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
+    if(valA != valB)
+            printf("Pregunta 6: Cierto \n");
+        else
+            printf("Pregunta 6: Falso \n");
+    
+    // Operadores Lógicos
+    
+    float valA2, valB2, valC2;
+    
+    valA2 = 5;
+    valB2 = valC2 = 10;
+    
+    if((valA2 < valB2) && (valB2 == valC2))
+        printf("Se cumplieron las dos condiciones \n");
+    else
+        printf("No se cumplieron las dos condiciones");
+    
+    if((valA2 > valB2) || (valA2 <= valC2))
+            printf("Se cumplieron por lo menos una de las dos condiciones \n");
+        else
+            printf("No se cumplieron las condiciones");
+        
 
 
     return 0;
