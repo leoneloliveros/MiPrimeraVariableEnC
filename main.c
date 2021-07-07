48c9e2970ef2ed01ecee6275e2fe8ebe39fd71be
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-07 11:37:21 -0500

    Challenge Resolved

--- main.c
+++ main.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
+    int limit = 9;
+    int prev = 1;
+    int next = 1;
+    int current = 0;
 
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
+    printf("\n%i", prev);
 
+    for(int i = 1; i < limit; i++)
+    {
+        current = prev + next;
+        prev = next;
+        next = current;
 
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
-
-
+        printf("\n%i", current);
+    }
     return 0;
 }
