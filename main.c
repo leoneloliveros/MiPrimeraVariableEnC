ede60fe5b5ab6c38cd49a698a3b411f49107d0db
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-07 07:45:51 -0500

    Learning For iterator

--- main.c
+++ main.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
+    printf("Iteradores For \n");
+    
+    int upperLim, bottomLim;
+    
+    printf("Imprimir en orden descendiente \n");
 
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
-
-
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
-
-
+    printf("Ingresar limite superior \n");
+    scanf("%i", &upperLim);
+    
+    printf("Ingresar limite inferior \n");
+    scanf("%i", &bottomLim);
+    
+    for(int i = upperLim; i >= bottomLim; i--)
+    {
+        printf("el numero es: %i \n", i);
+    }
+    
     return 0;
 }
