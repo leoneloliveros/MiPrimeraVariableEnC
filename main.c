62a3fa7eca307440af707a12ad898683e0f08f5d
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-08 22:48:04 -0500

    Studying strings

--- main.c
+++ main.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
+    printf("Cadena de caracteres \n");
 
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
-
-
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
-
+    char nameC[50];
+    
+    int size;
+    
+    printf("Ingresar el nombre con gets: \n");
+    
+    gets(nameC);
+    
+    printf("El nombre es: ");
+    puts(nameC);
+    
+    size = strlen(nameC);
+    
+    printf("\n El tamano de la cadena es: %i \n", size);
 
     return 0;
 }
