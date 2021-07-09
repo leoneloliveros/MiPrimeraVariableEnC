a39ffa591649be2ea7c3510575f33db9b4f25587
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-08 22:08:30 -0500

    Challenge Resolved

--- main.c
+++ main.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
-
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
-
-
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
-
-
+    int length = 0;
+    printf("Encontrar el número mas grande de un arreglo \n");
+    
+    printf("Tamano del arreglo: ");
+    scanf("%i", &length);
+    
+    int array[length];
+    int aux = 0;
+    
+    for (int i = 0; i < length; i++) {
+        printf("Valor[%i]: ", i);
+        scanf("%i", &array[i]);
+        if(array[i] > aux)
+            aux = array[i];
+    }
+    
+    printf("El numero mayor es: %i \n", aux);
+    
     return 0;
 }
