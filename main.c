0aa92659e4bce3cdcbcbba392f4f818fb8ff405a
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-08 21:24:52 -0500

    Studying Arrays

--- main.c
+++ main.c
@@ -4,24 +4,34 @@
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
-
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
-
-
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
-
-
+    printf("Arreglo bidimensionales \n");
+    
+    int bidimentionalArray[4][4];
+    
+    bidimentionalArray[0][0] = 11;
+    bidimentionalArray[0][1] = 12;
+    bidimentionalArray[0][2] = 13;
+    bidimentionalArray[0][3] = 14;
+    
+    bidimentionalArray[1][0] = 29;
+    bidimentionalArray[1][1] = 28;
+    bidimentionalArray[1][2] = 27;
+    bidimentionalArray[1][3] = 26;
+    
+    bidimentionalArray[2][0] = 36;
+    bidimentionalArray[2][1] = 35;
+    bidimentionalArray[2][2] = 34;
+    bidimentionalArray[2][3] = 33;
+    
+    bidimentionalArray[3][0] = 45;
+    bidimentionalArray[3][1] = 46;
+    bidimentionalArray[3][2] = 47;
+    bidimentionalArray[3][3] = 48;
+    
+    printf("Valor en (0,0): %i \n", bidimentionalArray[0][0]);
+    printf("Valor en (0,1): %i \n", bidimentionalArray[0][1]);
+    printf("Valor en (0,2): %i \n", bidimentionalArray[0][2]);
+    printf("Valor en (0,3): %i \n", bidimentionalArray[0][3]);
+    
     return 0;
 }
