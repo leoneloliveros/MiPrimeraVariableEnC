a029026cf71a5aacd3b348d8ef298240af9ac41f
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-08 20:38:45 -0500

    Studing One-dimensional Array

--- main.c
+++ main.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
-
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
-
-
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
-
-
+    printf("Arreglos Unididmensionales /n");
+    int integerList[3];
+    integerList[0] = 4;
+    integerList[1] = 5;
+    integerList[2] = 6;
+    
+    float floatList[] = {23.4, 34.5, 45.6, 21.8, 56.7};
+    
+    char charList[4];
+    
+    charList[0] = 'D';
+    charList[1] = 'a';
+    charList[2] = 'n';
+    charList[3] = 'i';
+    
+    printf("\n Primer entero:  \t\t %i", integerList[0]);
+    printf("\n Ultimo flotante:  \t\t %f", floatList[4]);
+    printf("\n Lista de caracteres:  \t\t %c%c%c%c", charList[0], charList[1], charList[2], charList[3]);
+    
     return 0;
 }
