9e80bad4150269f90a317770be97c7027e08685e
Leonel Oliveros <leoneloliveros.co@gmail.com>
2021-07-09 21:53:14 -0500

    chanllenge resolved

--- main.c
+++ main.c
@@ -2,26 +2,37 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+float convertion(float val, char option)
+{
+    float res;
+    if(option == 'P')
+    {
+        res = val * 3800;
+    }
+    else if (option == 'D')
+    {
+        res = val / 3800;
+    }
+    return res;
+}
+
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    double doubleA = 1.2;
-    char letterA = 'A';
-
-    // Forma de imprimir un valor en consola, se debe agregar %i
-    printf("El valor de nuestro entero A es: %i \n", integerA);
-
-
-    printf("El valor de nuestro float A es: %f \n", floatA);
-
-    printf("El valor de nuestro double A es: %f \n", doubleA);
-
-    printf("El valor de nuestro caracter A es: %c \n", letterA);
-
-
-
-
+    
+    char option;
+    float value;
+    
+    printf("Vamos a convertir cantidades de dinero \n");
+    
+    printf("Si quieres convertir de dolares a pesos presiona P \n");
+    printf("Si quieres convertir de pesos a dolares presiona D \n");
+    
+    scanf("%c", &option);
+    
+    printf("Escribe la cantidad que quieres cambiar: ");
+    scanf("%f", &value);
+    
+    printf("El resultado es: %f", convertion(value,option));
 
     return 0;
 }
